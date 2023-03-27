0c1873a945fc23ecc2dd56c8742365d84b726023
UE2020 <ttmmiizz101@gmail.com>
2023-03-26 20:24:41 -0400

    Use a fourth of the training data

--- src/nn.rs
+++ src/nn.rs
@@ -80,7 +80,7 @@ pub fn main() {
     };
 
     for result in rdr.records() {
-        if game > 5010000 {
+        if game > 1010000 {
             break;
         }
         let record = result.unwrap();
@@ -104,7 +104,7 @@ pub fn main() {
 			-eval
 		};
 
-        if game > 5000000 {
+        if game > 1000000 {
             test_positions.input.push(input);
             test_positions.labels.push(eval as f32 / 2000.0);
         } else {
